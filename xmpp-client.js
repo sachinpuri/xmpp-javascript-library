1969739280c3c4736dc9f11dab96a8a1679861e3
Sachin Puri <sachinpuri7@gmail.com>
2016-07-04 22:45:05 +0530

    onBlocked, onUnblocked event listener implemented

--- xmpp-client.js
+++ xmpp-client.js
@@ -28,6 +28,8 @@ var xmppClient={
     onRosterPush:null, //This function can be called multiple times for same user
     onRosterEntryRemove:null,
     onBlockListReceive:null,
+    onBlocked:null,
+    onUnblocked:null,
     bareJid:null,
     fullJid:null,
     archiveLimit:10,
@@ -191,7 +193,7 @@ xmppClient.roster = {
                                 .start('group').text(group).end()
                             .end()
                         .end()
-                    .end()
+                    .end();
         xmppClient.sendToServer(xmlGenerator.toString());
     },
     remove:function(jid){        
@@ -199,7 +201,7 @@ xmppClient.roster = {
                         .start('query', {xmlns:'jabber:iq:roster'})
                             .start('item', {jid:jid, subscription:'remove'}, true)
                         .end()
-                    .end()
+                    .end();
         
         xmppClient.sendToServer(xmlGenerator.toString());
         
@@ -336,7 +338,7 @@ var xmlGenerator = {
         
         return this;
     },
-    end:function(tagName){
+    end:function(){
         var tagName = this.stack.pop();
         this.tag += "</" + tagName + ">";
         return this;
@@ -422,9 +424,6 @@ var parser={
                     if(obj['subscription'] === "remove"){//Roster Entry Removed
                         xmppClient.onRosterEntryRemove(obj);
                     }else{
-//                        if($(xml).find("iq > query > item").attr("ask") !== undefined){//Presence Request
-//                            
-//                        }else{//Roster Entry
                             obj['name'] = $(xml).find("iq > query > item").attr("name");
                             if($(xml).find("iq > query > item > group").length>0){
                                 var group = $(xml).find("iq > query > item > group").text();
@@ -432,18 +431,20 @@ var parser={
                             obj['group'] = group;
                             obj['ask'] = $(xml).find("iq > query > item").attr("ask");
                             xmppClient.onRosterPush(obj);
-                        //}
-                        
                     }
                     
                 }else if($(xml).find("iq").attr("id")==="blocklist1"){
                     var arr = [];
                     $(xml).find("iq > blocklist > item").each(function() {
-                        if($(this).attr('jid').indexOf('@') !== -1)
+                        if($(this).attr('jid').indexOf('@') !== -1) {
                             arr.push($(this).attr('jid'));
-
+                        }
                     });
                     xmppClient.onBlockListReceive(arr);
+                }else if($(xml).find("iq > block").length > 0 && $(xml).find("iq > block").attr("xmlns") === "urn:xmpp:blocking"){
+                    xmppClient.onBlocked($(xml).find("iq > block > item").attr("jid"));
+                }else if($(xml).find("iq > unblock").length > 0 && $(xml).find("iq > unblock").attr("xmlns") === "urn:xmpp:blocking"){
+                    xmppClient.onUnblocked($(xml).find("iq > unblock > item").attr("jid"));
                 }
                 break;
             case "PRESENCE":    
@@ -522,15 +523,9 @@ var parser={
             objMessage['type'] = 'archive';
 
             if($(xml).find("delay").length>0){
-                var date = $(xml).find("delay").attr("stamp").substr(0,10).split('-');
-                var time = $(xml).find("delay").attr("stamp").substr(11,8).split(':');
-                var dateTime = new Date(date[0], date[1]-1, date[2], time[0], time[1], time[2]);
-                dateTime.setTime(dateTime.getTime() + (5.5 * 60 * 60 * 1000));
-                
-                //console.log(dateTime.getHours() + ':' + dateTime.getMinutes() + ':' + dateTime.getSeconds());
                 objMessage['delay'] = {};
                 objMessage['delay']['text'] = $(xml).find("delay").text();
-                objMessage['delay']['time'] = dateTime.toLocaleString().replace(new RegExp("/",'g'),"-");
+                objMessage['delay']['time'] = $(xml).find("delay").attr("stamp");
             }
 
             xmppClient.onMessageReceive(objMessage);
@@ -619,12 +614,8 @@ var parser={
     }
 };
 
-function log(){
-    if(arguments.length === 1){
-    console.log(arguments[0]);
-}else{
-    console.log(arguments[0] + ": " + arguments[1]);
-    }
+function log(tag, data){
+    if(xmppClient.debug) console.log(tag + ": " , data);
 }
 
 var Base64 = {_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", encode:function(e){var t = ""; var n, r, i, s, o, u, a; var f = 0; e = Base64._utf8_encode(e); while (f < e.length){n = e.charCodeAt(f++); r = e.charCodeAt(f++); i = e.charCodeAt(f++); s = n >> 2; o = (n & 3) << 4 | r >> 4; u = (r & 15) << 2 | i >> 6; a = i & 63; if (isNaN(r)){u = a = 64} else if (isNaN(i)){a = 64}t = t + this._keyStr.charAt(s) + this._keyStr.charAt(o) + this._keyStr.charAt(u) + this._keyStr.charAt(a)}return t}, decode:function(e){var t = ""; var n, r, i; var s, o, u, a; var f = 0; e = e.replace(/[^A-Za-z0-9\+\/\=]/g, ""); while (f < e.length){s = this._keyStr.indexOf(e.charAt(f++)); o = this._keyStr.indexOf(e.charAt(f++)); u = this._keyStr.indexOf(e.charAt(f++)); a = this._keyStr.indexOf(e.charAt(f++)); n = s << 2 | o >> 4; r = (o & 15) << 4 | u >> 2; i = (u & 3) << 6 | a; t = t + String.fromCharCode(n); if (u != 64){t = t + String.fromCharCode(r)}if (a != 64){t = t + String.fromCharCode(i)}}t = Base64._utf8_decode(t); return t}, _utf8_encode:function(e){e = e.replace(/\r\n/g, "\n"); var t = ""; for (var n = 0; n < e.length; n++){var r = e.charCodeAt(n); if (r < 128){t += String.fromCharCode(r)} else if (r > 127 && r < 2048){t += String.fromCharCode(r >> 6 | 192); t += String.fromCharCode(r & 63 | 128)} else{t += String.fromCharCode(r >> 12 | 224); t += String.fromCharCode(r >> 6 & 63 | 128); t += String.fromCharCode(r & 63 | 128)}}return t}, _utf8_decode:function(e){var t = ""; var n = 0; var r = c1 = c2 = 0; while (n < e.length){r = e.charCodeAt(n); if (r < 128){t += String.fromCharCode(r); n++} else if (r > 191 && r < 224){c2 = e.charCodeAt(n + 1); t += String.fromCharCode((r & 31) << 6 | c2 & 63); n += 2} else{c2 = e.charCodeAt(n + 1); c3 = e.charCodeAt(n + 2); t += String.fromCharCode((r & 15) << 12 | (c2 & 63) << 6 | c3 & 63); n += 3}}return t}};
